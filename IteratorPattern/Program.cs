9042bdf42ae735647d5a89382e6bc85c66a217a7
DxMort <99000786+DxMort@users.noreply.github.com>
2024-12-02 11:15:49 +0700

    Add files via upload

---
+++ IteratorPattern/Program.cs
@@ -0,0 +1,18 @@
+﻿using IteratorPattern;
+using System.Collections;
+
+class Program
+{
+    static void Main()
+    {
+        Library library = new Library();
+        IEnumerator librarian = library.GetEnumerator();
+
+        library.AddBook(new Book("Пособие для сна", "Книга Матвей", 2023));
+        library.AddBook(new Book("Гойда братья", "Карпенко Егор", 2024));
+        library.AddBook(new Book("Design Patterns via C#", "Александр Шевчук, Дмитрий Охрименко, Андрей Касьянов", 2015));
+        
+        while (librarian.MoveNext())
+            Console.WriteLine(((Book)librarian.Current).Title);
+    }
+}
